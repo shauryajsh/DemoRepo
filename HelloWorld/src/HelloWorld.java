c09cc809ef5629368227e62c7cb90f79fe3adf8c
Shaurya <Shaurya@DESKTOP-ILU7BSN>
2021-02-23 10:31:15 +0530

    Commiting the updated file

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,7 +2,9 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.print("Hello World");
+		System.out.println("Hello World");
+		
+		System.out.println("Next line");
 	}
 
 }
